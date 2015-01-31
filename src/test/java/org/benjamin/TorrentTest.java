21d81a385eeb198ac2a6177a335de8b31d407343
Andrew Barchuk <raindev@icloud.com>
2015-01-31 21:03:29 +0200

    Finish Torrent decoding test
    Verify decoding of the rest of Torrent file.
    
    Change decoding charset to ASCII from UTF-8 to make raw strings
    original bytes recovery (pieces hashes, in particular). Right now if
    not bijective character encoding is used in decoder (e.g. UTF-8),
    retrieving of original bytes aren't possible.

--- src/test/java/org/benjamin/TorrentTest.java
+++ src/test/java/org/benjamin/TorrentTest.java
@@ -7,8 +7,9 @@ import java.io.InputStream;
 import java.io.IOException;
 import java.util.Map;
 
-import static org.testng.Assert.*;
-import static java.nio.charset.StandardCharsets.UTF_8;
+import static java.nio.charset.StandardCharsets.US_ASCII;
+import static java.util.Arrays.asList;
+import static org.testng.Assert.assertEquals;
 
 @Test
 public class TorrentTest {
@@ -19,14 +20,29 @@ public class TorrentTest {
     @BeforeMethod
     void setUp() {
         stream = getClass().getResourceAsStream("/ubuntu-14.10-desktop-amd64.iso.torrent");
-        decoder = new Bdecoder(UTF_8, stream);
+        decoder = new Bdecoder(US_ASCII, stream);
     }
 
     @Test
+    @SuppressWarnings("unchecked")
     void decodeTorrent() throws IOException {
         Map<String, ?> torrent = decoder.readDictionary();
-
         assertEquals(torrent.get("announce"), "http://torrent.ubuntu.com:6969/announce");
         assertEquals(torrent.get("comment"), "Ubuntu CD releases.ubuntu.com");
+        assertEquals(
+                torrent.get("announce-list"),
+                asList(
+                    // first announce tier
+                    asList("http://torrent.ubuntu.com:6969/announce"),
+                    // backup announce tier
+                    asList("http://ipv6.torrent.ubuntu.com:6969/announce")));
+        assertEquals(torrent.get("creation date"), 1414070124L);    // 10.23.2014 1:15pm
+
+        Map<String, ?> info = (Map<String, ?>) torrent.get("info");
+        assertEquals(info.get("length"), 1162936320L);  // 1.08 Gb
+        assertEquals(info.get("name"), "ubuntu-14.10-desktop-amd64.iso");
+        assertEquals(info.get("piece length"), 524288L);
+        // concatenation of hashsums of the file pieces
+        assertEquals(((String) info.get("pieces")).getBytes(US_ASCII).length, 44380);
     }
 }
