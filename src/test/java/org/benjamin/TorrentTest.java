f51c812d78b9d536bb0d59682d7949b37fa88860
Andrew Barchuk <raindev@icloud.com>
2015-08-31 00:00:24 +0300

    Enable and make pass TorrentTest
    Change Torrent file decoding code accordingly to changed dictionary
    decoding logic: all strings are now decoded as raw byte arrays to
    prevent data loss.
    
    In progress issue #1

--- src/test/java/org/benjamin/TorrentTest.java
+++ src/test/java/org/benjamin/TorrentTest.java
@@ -10,6 +10,7 @@ import java.util.Map;
 import static java.nio.charset.StandardCharsets.US_ASCII;
 import static java.util.Arrays.asList;
 import static org.testng.Assert.assertEquals;
+import static org.unitils.reflectionassert.ReflectionAssert.assertReflectionEquals;
 
 @Test
 public class TorrentTest {
@@ -23,26 +24,31 @@ public class TorrentTest {
         decoder = new Bdecoder(US_ASCII, stream);
     }
 
-    @Test(enabled = false)
+    @Test
     @SuppressWarnings("unchecked")
     void decodeTorrent() throws IOException {
         Map<String, ?> torrent = decoder.readDictionary();
-        assertEquals(torrent.get("announce"), "http://torrent.ubuntu.com:6969/announce");
-        assertEquals(torrent.get("comment"), "Ubuntu CD releases.ubuntu.com");
-        assertEquals(
+        assertEquals(torrent.get("announce"), "http://torrent.ubuntu.com:6969/announce"
+                .getBytes("UTF-8"));
+        assertEquals(torrent.get("comment"), "Ubuntu CD releases.ubuntu.com"
+                .getBytes("UTF-8"));
+        assertReflectionEquals(
                 torrent.get("announce-list"),
                 asList(
                     // first announce tier
-                    asList("http://torrent.ubuntu.com:6969/announce"),
+                    asList("http://torrent.ubuntu.com:6969/announce"
+                        .getBytes("UTF-8")),
                     // backup announce tier
-                    asList("http://ipv6.torrent.ubuntu.com:6969/announce")));
+                    asList("http://ipv6.torrent.ubuntu.com:6969/announce"
+                        .getBytes("UTF-8"))));
         assertEquals(torrent.get("creation date"), 1414070124L);    // 10.23.2014 1:15pm
 
         Map<String, ?> info = (Map<String, ?>) torrent.get("info");
         assertEquals(info.get("length"), 1162936320L);  // 1.08 Gb
-        assertEquals(info.get("name"), "ubuntu-14.10-desktop-amd64.iso");
+        assertEquals(info.get("name"), "ubuntu-14.10-desktop-amd64.iso"
+                .getBytes("UTF-8"));
         assertEquals(info.get("piece length"), 524288L);
         // concatenation of hashsums of the file pieces
-        assertEquals(((String) info.get("pieces")).getBytes(US_ASCII).length, 44380);
+        assertEquals(((byte[]) info.get("pieces")).length, 44380);
     }
 }
